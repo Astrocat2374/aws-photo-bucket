77a8840673774a4256de4b44ed9a9def73b06f42
Astrocat2374 <sugiyama.evan@gmail.com>
2018-11-16 14:15:03 -1000

    Added function

---
+++ lambdas/dig.js
@@ -0,0 +1,32 @@
+'use strict';
+const AWS = require('aws-sdk')
+const s3 = new AWS.S3();
+
+module.exports.dig = async (event, context, callback) => {
+      
+  var params = {
+    Bucket: "treasure-chest-capstone"
+  }
+
+  s3.listObjects(params, function(err, data){
+    if (err) {
+      console.log(err);
+    }
+      
+    console.log('data', data);
+    const response =  {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true
+      },
+      body: JSON.stringify({
+        message: data,
+      }),
+    };
+    callback(null, response)
+  })
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
