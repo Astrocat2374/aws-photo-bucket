916e200a130938f6cfd4376a86be9ba370914b60
Astrocat2374 <sugiyama.evan@gmail.com>
2018-11-21 14:32:41 -1000

    Jarren was here

--- public/app.js
+++ public/app.js
@@ -1,25 +1,18 @@
-/*axios
-    .get('https://cgq9e5xwrg.execute-api.us-west-2.amazonaws.com/dev/hole')
-    .then((response)=>{
-        console.log('response', response.data.message.Contents);
-    })
-    .catch((error)=>{
-        console.log('error', error);
-});
-*/
-
-
-
-
-
+const submit = document.querySelector('#render').addEventListener('click', renderResults);
 
 function renderResults(){
     axios
     .get(`https://cgq9e5xwrg.execute-api.us-west-2.amazonaws.com/dev/hole`)
-    .then((response).map(response => {
-        console.log(
-            response
-        )
+    .then((response).data.message.Contents.map(x => {
+        console.log(x);
+        /*let cardTemplate = `
+            <div class = 'card'>
+                <img src = 'https://s3-us-west-2.amazonaws.com/treasure-chest-capstone/${x.key}'
+            </div>
+        `
+        let newCard = document.createElement('div');
+        cardTemplate.appendChild(newCard).className='card';
+        newCard.innerHTML = cardTemplate;*/
     }))
     .catch((error)=>{
         console.log(error);
